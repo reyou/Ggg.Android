d064cd7856189ebe021a2cd055895fd73b3314e9
Teoman Shipahi <aytekoz@yahoo.com>
2019-05-11 09:54:04 -0400

    kotlin

---
+++ apps/app-books/professional-android/README.md
@@ -0,0 +1,3 @@
+### Symlink
+
+mklink /D c:\professional-android D:\Git\Ggg.Github\Ggg.Android\apps\app-books\professional-android\
---
+++ apps/app-books/professional-android/chap2-getting-started/intro3-kotlin/app/src/main/java/com/foodception/dj_re/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.foodception.dj_re.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
