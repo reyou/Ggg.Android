3a6e07fa39919f8399198fa9701097db80b62fc9
Teoman Shipahi <aytekoz@yahoo.com>
2020-07-16 01:14:03 -0500

    GradleMigrationSettings

--- apps/apps-web/thetechnocafe.com/understanding-lifecycle-in-android-architecture-components/intro1/app/src/main/java/com/readendless/learning/understandinglifecycleinandroid/MainActivity.kt
+++ apps/apps-web/thetechnocafe.com/understanding-lifecycle-in-android-architecture-components/intro1/app/src/main/java/com/readendless/learning/understandinglifecycleinandroid/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.readendless.learning.understandinglifecycleinandroid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var mTextView = findViewById<TextView>(R.id.textView)
         var mButton = findViewById<Button>(R.id.button)
         var mMutableLiveData = MutableLiveData<String>()
+        mMutableLiveData.observe(this,
+            Observer { liveData ->
+                mTextView.text = liveData
+            })
+        mButton.setOnClickListener {
+            val random = Random()
+            mMutableLiveData.value = "Random Int:" + random.nextInt()
+        }
     }
 }
