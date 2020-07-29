62eabe0e73592ca59ba75ef9bf91ea61f483d21d
Teoman Shipahi <aytekoz@yahoo.com>
2020-07-28 22:33:47 -0500

    rolButton

--- apps/apps-web/classroom.udacity.com/with-kotlin/intro1/app/src/main/java/com/readendless/learning/diceroller/MainActivity.kt
+++ apps/apps-web/classroom.udacity.com/with-kotlin/intro1/app/src/main/java/com/readendless/learning/diceroller/MainActivity.kt
@@ -2,10 +2,26 @@ package com.readendless.learning.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val random = Random()
+        val randomInt = random.nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
